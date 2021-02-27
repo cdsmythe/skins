dfd1da4fd857fc79e8ab32475932dd3f4e6c03f0
Campbell Smythe <smythec@gmail.com>
2021-02-27 16:19:13 +1000

    Add files via upload

---
+++ minecraft/build/precache-manifest.e12f8493b12fdcc38fa206b56369a47d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6828fd450a379fd72dee0442b9555abc",
+    "url": "/minecraftskins/index.html"
+  },
+  {
+    "revision": "f0abc9ec3b160fdcd713",
+    "url": "/minecraftskins/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c9f9522e5cfbdcea0cb2",
+    "url": "/minecraftskins/static/css/main.41653c8c.chunk.css"
+  },
+  {
+    "revision": "f0abc9ec3b160fdcd713",
+    "url": "/minecraftskins/static/js/2.cfd3ebca.chunk.js"
+  },
+  {
+    "revision": "f58650fcb154fe1c99d724852b659e19",
+    "url": "/minecraftskins/static/js/2.cfd3ebca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f9522e5cfbdcea0cb2",
+    "url": "/minecraftskins/static/js/main.aca7226a.chunk.js"
+  },
+  {
+    "revision": "ef81f64a1d1150958e0b",
+    "url": "/minecraftskins/static/js/runtime-main.38a3d76b.js"
+  }
+]);
